2fd14570f7b3180d13745527a39d17b28c9b3b7c
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2024-12-29 16:07:34 -0400

    adding js file

---
+++ main.js
@@ -0,0 +1,8 @@
+//Block function format
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+//Arrow function format
+//Subtract the two known angles from 180 to give you the third angle
+const otherAngle = (a, b) => 180 - (a + b);
